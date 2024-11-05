a1b633a9440ae3217ec46d71ecce222ffc339945
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-11-05 19:42:50 +0200

    set up tailwindcss

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(GZipMiddleware)
 templates = Jinja2Templates(directory="templates")
 
 
