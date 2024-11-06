657cc3c4ec5538bc82d125acb659ba82a741b2ca
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-11-06 11:35:33 +0200

    add math package & calculus endpoint

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.gzip import GZipMiddleware
 
+from _math.solver import do_math
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -13,3 +15,14 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/hello")
 def main(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
+
+
+@app.get("/calculus-1")
+def calculus(request: Request):
+    equation, rs = do_math()
+
+    return templates.TemplateResponse(
+        request=request,
+        name="calculus.html",
+        context={"equation": equation, "roots": rs},
+    )
