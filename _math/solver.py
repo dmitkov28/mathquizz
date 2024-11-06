c412a4bc910e847090eb8ba8a51a84879e4f00e5
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-11-06 20:37:53 +0200

    remove solver

--- _math/solver.py
+++
@@ -1,8 +0,0 @@
-from sympy import Eq, latex, solve, symbols
-
-
-def do_math():
-    x = symbols("x")
-    equation = latex(Eq(x**2 - 4, 0))
-    rs = solve(Eq(x**2 - 4, 0))
-    return equation, rs
